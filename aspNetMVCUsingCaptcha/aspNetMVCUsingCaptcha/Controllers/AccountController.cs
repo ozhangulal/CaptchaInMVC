732492eec4c0751e1c4ceb518cb7dec2a671b074
ozhangulal <ozhangulal@outlook.com>
2019-07-25 10:35:01 +0300

    asp.NET MVC içerisinde klasik captcha kullanımı/using classic captcha in asp.NET MVC
    asp.net içerisinde nuget üzerinden faydalanılarak klasik captcha kullanimi. EN- inside of asp.NET thanks to nuget how to use classic captcha.

---
+++ aspNetMVCUsingCaptcha/aspNetMVCUsingCaptcha/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaptchaMvc.HtmlHelpers;
+using aspNetMVCUsingCaptcha.Models;
+
+namespace aspNetMVCUsingCaptcha.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var account = new Account();
+            return View("Index",account);
+        }
+        [HttpPost]
+        public ActionResult Save(Account account)
+        {
+
+            if (!this.IsCaptchaValid(""))
+            {
+                ViewBag.error = "Invalid Captcha";
+                return View("Index",account);
+            }
+            else
+            {
+                ViewBag.account = account;
+                return View("Success",account);
+            }
+
+        }
+    }
+}
---
+++ aspNetMVCUsingCaptcha/aspNetMVCUsingCaptcha/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aspNetMVCUsingCaptcha.Models
+{
+    public class Account
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+
+    }
+}
